2d01a1492c22e66dc99d0cdaaae392418220bd21
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-10 23:29:34 +0100

    commit tree cf6ead3ab3e859e698b44ac32f19a5f23c847707

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+
+function saturdayFun(activity = 'roller-skate') {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(mondayWork = 'go to the office') {
+    return `This Monday, I will ${mondayWork}.`
+}
+
+function wrapAdjective(star = "*") {
+    return function(phrase = "special"){
+        return `You are ${star}${phrase}${star}!`
+    }}
